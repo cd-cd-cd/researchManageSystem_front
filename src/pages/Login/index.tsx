ac653e968142a93c3e1546b1ecccb337b884bf9c
cd-cd-cd <1262877491@qq.com>
2023-01-03 15:56:25 +0800

    feat: login

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,15 +1,83 @@
 import React from 'react'
 import style from './index.module.scss'
 import headerIcon from '../../assets/imgs/science_icon.svg'
+import { Form, Radio } from 'antd'
+import Input from 'antd/lib/input'
+import Button from 'antd/lib/button'
 
 export default function Login () {
+  const [form] = Form.useForm()
+  const onFinish = (values: any) => {
+    console.log('Success:', values)
+  }
+
+  const onFinishFailed = (errorInfo: any) => {
+    console.log('Failed:', errorInfo)
+  }
+
+  const onReset = () => {
+    form.resetFields()
+  }
+
   return (
     <div className={style.login_part}>
       <header className={style.header}>
         <img src={headerIcon} className={style.icon}></img>
+        科研团队管理系统
       </header>
       <main className={style.main}>
         <div className={style.login_box}>
+          <Form
+            className={style.login_form}
+            form={form}
+            name='login'
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+          >
+            <Form.Item
+              label='选择角色'
+              name='role'
+              rules={[
+                { required: true, message: '请选择角色' }
+              ]}
+              initialValue={0}
+            >
+              <Radio.Group>
+                <Radio value={0}>学生</Radio>
+                <Radio value={1}>老师</Radio>
+                <Radio value={2}>管理员</Radio>
+              </Radio.Group>
+            </Form.Item>
+            <Form.Item
+              name='username'
+              rules={[
+                { required: true, message: '用户名不为空' }
+              ]}
+            >
+              <Input placeholder='请输入用户名'>
+              </Input>
+            </Form.Item>
+            <Form.Item
+              name='password'
+              rules={[
+                { required: true, message: '密码不为空' }
+              ]}
+            >
+              <Input.Password placeholder='请输入密码'></Input.Password>
+            </Form.Item>
+            <Form.Item>
+              <div className={style.buttons}>
+                <Button className={style.button} type='primary' htmlType="submit">登录</Button>
+                <Button className={style.button} htmlType="button" onClick={onReset}>重置</Button>
+              </div>
+            </Form.Item>
+            <Form.Item>
+              <div>
+                <p className={style.warning_text}>本网站禁止爬虫采集或转载商业化</p>
+                <p>本网站建议使用谷歌浏览器</p>
+              </div>
+            </Form.Item>
+          </Form>
         </div>
       </main>
       <footer className={style.footer}>科研团队管理系统 - Scientific research team management system - 2023</footer>
